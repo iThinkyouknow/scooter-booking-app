f2afa9a668f3594a6a981357fee860f54491121d
iThinkyouknow <woontlt.dev@gmail.com>
2019-02-25 13:44:41 +0800

    made host location based on configs

--- fetch/api.js
+++ fetch/api.js
@@ -1,12 +1,14 @@
 
+const serverConfigs = require('../constants/serverConfigs')
+
 const api_getScooters = ({ latitude, longitude }) => {
-    return fetch(`http://localhost:3000/scooters?latitude=${latitude}&longitude=${longitude}`)
+    return fetch(`http://${serverConfigs.host}/scooters?latitude=${latitude}&longitude=${longitude}`)
         .then(res => res.json())
         .catch((err) => console.warn(`Couldn't fetch from scooters api with ${err}`));
 };
 
 const api_getPreviousRides = () => {
-    return fetch('http://localhost:3000/history')
+    return fetch(`http://${serverConfigs.host}/history`)
         .then(res => res.json())
         .catch((err) => console.warn(`Couldn't fetch from history api with ${err}`));
 }
